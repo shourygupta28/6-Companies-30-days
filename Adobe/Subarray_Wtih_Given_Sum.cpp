a39d6355b01c9e65d2a5189da6d8694fc659c344
Shourya Gupta <54794237+shourygupta28@users.noreply.github.com>
2022-02-02 17:28:53 +0530

    Create Subarray_Wtih_Given_Sum.cpp

---
+++ Adobe/Subarray_Wtih_Given_Sum.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+
+        int l = 0, r = 0;
+        long long sum = arr[r];
+        vector<int> res;
+
+        while (l <= r && r < n && l < n)
+        {
+            while (sum > s && l < n)
+            {
+                sum -= arr[l];
+                l++;
+            }
+            if (sum == s)
+            {
+                res = {l + 1, r + 1};
+                return res;
+            }
+
+            if (r < n)
+            {
+                r++;
+                sum += arr[r];
+            }
+        }
+
+        res.push_back(-1);
+
+        return res;
+    }
+};
